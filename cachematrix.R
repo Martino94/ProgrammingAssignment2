f05b85b560158ea48a0e27f52675c43ad8bf9560
Martino94 <85128365+Martino94@users.noreply.github.com>
2021-08-19 11:16:32 -0500

    Update cachematrix.R
    Se realizaron los cambios pedidos en la asignación dos del curso R programming

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,45 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
-
+# Creo la función
+makeCacheMatrix <- function(x=matrix()) {
+  # COnfiguro la matriz
+  m <- NULL
+  set <- function(matrix) {
+    x <<- matrix
+    m <<- NULL
+  }
+# Forma en la que obtengo la matriz
+  get <- function() x
+# Inversa de la matriz  
+   setinvm <- function(solve) {
+    m <<- solve
+  }
+  
+  getinvm <- function() m
+  # recupero los resultados
+       list(set = set, get = get,
+       setinvm = setinvm,
+       getinvm = getinvm)
 }
 
-
-## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  
+  m <- x$getinvm
+  
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  
+  data <- x$get()
+  
+  m <- solve(data) %*% data
+  
+  x$setinvm(m)
+  
+  ## Imprimo la matriz
+  m
 }
+
